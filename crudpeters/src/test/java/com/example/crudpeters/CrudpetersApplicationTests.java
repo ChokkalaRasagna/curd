7e3429655d8928960347d0d5cf3497aeba5cfa1b
ChokkalaRasagna <166834580+ChokkalaRasagna@users.noreply.github.com>
2025-11-21 16:18:26 +0530

    Add files via upload

---
+++ crudpeters/src/test/java/com/example/crudpeters/CrudpetersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudpeters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpetersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
